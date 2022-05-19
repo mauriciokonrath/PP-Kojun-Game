b1ea8f5777f11b08831a0c8d3aef353fb2cfd4ac
nicol4ss <nicolaselias@pm.me>
2022-05-19 09:20:49 -0300

    Transcrevendo algumas das funcoes de python para haskell

--- matrix.hs
+++ matrix.hs
@@ -1,6 +1,16 @@
-newMatrix1 :: Integer
-newMatrix1 = 
+newMatrix :: [[Int]]
+newMatrix = 
     [0,0,0,0,0,0]:[0,0,0,0,0,0]:[0,0,0,0,0,0]:[0,0,0,0,0,0]:[0,0,0,0,0,0]:[0,0,0,0,0,0]:[]
 
-main = do
-    print newMatrix1
+searchEmpty :: [[Int]] -> (Int)
+searchEmpty m =
+    -- imcompleto
+
+set :: [[Int]] -> [Int] -> Int -> [[Int]]
+set mat coord value =
+    mat[coord[0]][coord[1]] = value
+    mat
+
+get :: [[Int]] -> [Int] -> Int
+get mat coord =
+    mat[coord[0]][coord[1]]
