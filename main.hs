34f84c2b1bba078feff4de6d9fe263d2c6cc6424
anthongretter <anthonporathgretter@yahoo.com.br>
2022-05-23 14:50:13 -0300

    criando os modulos Matrix e Generic

--- main.hs
+++ main.hs
@@ -1,4 +1,12 @@
+-- principal
+
+-- pontos para se perguntar para o professor:
+--  * vai existir algum conflito se o nome do modulo ser o mesmo que o de um tipo dentro do mesmo?
+--  * quando for realizar o import, e o modulo nao se encontra no mesmo diretorio,
+--    precisa especificar o caminho (igual ao python)?
 
 
 main = do
-    --Inicio da execução
+  let m = newMatrix 2 2
+  print(m)
+  print(set m (1,1) 2)
