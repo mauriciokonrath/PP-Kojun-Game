cdf2147d8811fa46e3e3241055798d47ef71ca23
Mauricio Konrath <83033652+mauriciokonrath@users.noreply.github.com>
2022-05-26 08:42:36 -0300

    Ideias de como checar se existe solução

---
+++ src/checarSolucao.hs
@@ -0,0 +1,12 @@
+-- Módulo para verificação de uma solução
+
+
+-- verificar se a lista de elementos de cada grupo está na forma correta
+
+-- verifica se cada grupo possui elementos repetidos
+
+-- verifica se cada elemento da solução possui valores adjacentes iguais a ele
+
+-- verifica se células inferiores possuem valores menores e se pertencentes ao mesmo grupo
+
+-- verifica se a matriz é a solução
