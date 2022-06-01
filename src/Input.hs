97c86a41afe06389d30b4fc1d64d2e3be6e00420
Nicolas Elias <nicolaselias@pm.me>
2022-06-01 03:57:26 -0300

    Alterando possibilities

--- src/Input.hs
+++ src/Input.hs
@@ -5,4 +5,12 @@ input = [
   [2, 2, 3, 4, 4],
   [2, 3, 3, 4, 4],
   [2, 3, 3, 5, 4],
-  [2, 5, 5, 5, 5]]
+  [2, 5, 5, 5, 4]]
+
+-- Percorre todas os quadrantes para medir o tamanho deles para ter opcoes do que colocar nele
+possibilities = [
+  [1,2,3,4,5],
+  [1,2,3,4,5],
+  [1,2,3,4,5],
+  [1,2,3,4,5,6],
+  [1,2,3,4]]
--- src/solver/Possibilities.hs
+++
@@ -1,5 +0,0 @@
-module Possibilities where
-
--- Percorre todas os quadrantes para medir o tamanho deles para ter opcoes do que colocar nele
-possibilities:: -- definir entrada
-possibilities =
